ac0c5f646f8b9bc90458c7dde1db5c3ac53c9c7c
tinojona <wind.tino@gmail.com>
2025-03-21 11:38:09 +0100

    ui of plots and table

--- ui.R
+++ ui.R
@@ -1,22 +1,38 @@
-# Define UI for application that draws a histogram
+# user interface
 ui <- fluidPage(
   
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  # title
+  titlePanel("Individual Level Data Screening"),
   
-  # Sidebar with a slider input for number of bins
+  # Sidebar with date range input
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      dateRangeInput("date_range", 
+                     label = "Select a time period:", 
+                     start = ymd("2024-09-27"),  # Default start date
+                     end = Sys.time()), # Default end date
+      
+      # Dropdown for selecting UID
+      selectInput("uid_select", 
+                  "Select Participant for Ploting:", 
+                  choices = NULL,  # This will be dynamically populated
+                  selected = NULL),
+      textOutput("file_path"),
+      
+      # Display the list of files in the folder
+      uiOutput("file_list_ui"),
+      
+      # Display the total number of files
+      textOutput("total_files")
     ),
     
-    # Show a plot of the generated distribution
+    # Main panel displaying selected date range and filtered data
     mainPanel(
-      plotOutput("distPlot")
+      tableOutput("filtered_data"),
+      uiOutput("data_plot")
     )
   )
 )
+
+
+
